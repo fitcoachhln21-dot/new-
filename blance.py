d7f321509fc0ce4f1ae7e1a74aca5f879271a164
Harshith <fitcoachhln21@gmail.com>
2026-09-09 15:43:13 +0530

    save

---
+++ blance.py
@@ -0,0 +1,8 @@
+intial_balance = float(input("Enter Intial Balance: "))
+deposit = float(input("Enter Amount to Deposit: "))
+
+blance = intial_balance + deposit
+
+print("Intial Balance: ", intial_balance)
+print("Amount to Deposit: ", deposit)
+print("your balance is :", blance)

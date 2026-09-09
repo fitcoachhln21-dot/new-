8585113864d852ed00bc57da0e962d6e0ab410e6
fitcoachhln21-dot <fitcoachhln21@gmail.com>
2026-09-09 16:12:08 +0530

    Update blance.py

--- blance.py
+++ blance.py
@@ -3,6 +3,9 @@ deposit = float(input("Enter Amount to Deposit: "))
 
 blance = intial_balance + deposit
 withdraw_amt = float(input("enter the withdraw amt"))
+new_blance = blance-withdraw_amt
+                   
 print("Intial Balance: ", intial_balance)
 print("Amount to Deposit: ", deposit)
 print("your balance is :", blance)  
+
